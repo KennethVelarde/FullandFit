4db605667b950ef587c3a37a674239413e37a9be
OneTallProgrammer <jmcauthen@live.com>
2019-10-23 15:25:11 -0700

    Added sliding panel in the menu page

---
+++ fullandfit/static/js/menu.js
@@ -0,0 +1,9 @@
+// set the width of the sidebar to 250px (show it)
+function open_nutrition(){
+    document.getElementById("nutrition_parameters").style.width = "75em";
+}
+
+// set the width of the sidebar to 0px (hide it)
+function close_nutrition(){
+    document.getElementById("nutrition_parameters").style.width = "0px";
+}
--- fullandfit/views.py
+++ fullandfit/views.py
@@ -30,6 +30,10 @@ def menu_page(request):
     test_menu = dict()
     test_menu[0] = {"id": 0, "name": "burger", "price": 5.99, "calories": 700, "carbs": 60, "protein": 30, "fat": 10}
     test_menu[1] = {"id": 1, "name": "fries", "price": 2.50, "calories": 100, "carbs": 10, "protein": 0, "fat": 15}
+    test_menu[2] = {"id": 2, "name": "coke", "price": 1.99, "calories": 150, "carbs": 60, "protein": 0, "fat": 0}
+    test_menu[3] = {"id": 3, "name": "milkshake", "price": 2.99, "calories": 400, "carbs": 100, "protein": 20, "fat": 15}
+    test_menu[4] = {"id": 4, "name": "chicken sandwich", "price": 6.99, "calories": 615, "carbs": 60, "protein": 30, "fat": 15}
+    test_menu[5] = {"id": 5, "name": "salad", "price": 4.99, "calories": 100, "carbs": 0, "protein": 0, "fat": 15}
 
     items = []
 
