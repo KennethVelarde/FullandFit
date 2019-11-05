727a7431d2ff5cc873adb55d68b9f48a7ebaf6d3
Nutella399 <nutella399@gmail.com>
2019-11-04 17:11:41 -0800

    Created models for each of the 4 resturants we are doing.

--- FullandFitApp/models.py
+++ FullandFitApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Carls(models.Model): 
+	item = models.CharField(max_length=120) 
+	id = models.IntegerField()
+	price = models.FloatField()
+	calories = models.FloatField()
+	carbs = models.FloatField()
+	protein = models.FloatField()
+	fat = models.FloatField()
+	
+class TacoBell(models.Model): 
+	item = models.CharField(max_length=120) 
+	id = models.IntegerField()
+	price = models.FloatField()
+	calories = models.FloatField()
+	carbs = models.FloatField()
+	protein = models.FloatField()
+	fat = models.FloatField()	
+	
+class Den(models.Model): 
+	item = models.CharField(max_length=120) 
+	id = models.IntegerField()
+	price = models.FloatField()
+	calories = models.FloatField()
+	carbs = models.FloatField()
+	protein = models.FloatField()
+	fat = models.FloatField()
+	
+class RoundTable(models.Model): 
+	item = models.CharField(max_length=120) 
+	id = models.IntegerField()
+	price = models.FloatField()
+	calories = models.FloatField()
+	carbs = models.FloatField()
+	protein = models.FloatField()
+	fat = models.FloatField()
