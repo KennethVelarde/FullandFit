45d4886a373487bf5a7c9ef159c237f3714b5d1e
Nutella399 <nutella399@gmail.com>
2019-11-11 14:52:23 -0800

    change to a list of dictionaries for easier use and fixed the issue with db.sqlite3

--- FullandFitApp/reads.py
+++ FullandFitApp/reads.py
@@ -4,37 +4,23 @@ conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 
 def read_from_db(var):
-<<<<<<< HEAD
-	menu = {}
+	menu = []
 	c.execute('SELECT * FROM ' + var)
-	#data = c.fetchall()
-	#print(data)
 	for column in c.fetchall():
 		item = {}
+		id = 0
 		item["name"] = column[0]
 		item["price"] = column[1]
 		item["calories"] = column[2]
 		item["carbs"] = column[3]
 		item["protein"] = column[4]
 		item["fat"] = column[5]
-		menu[column[0]] = item
-		#print(menu) 
-		#print(item)
+		menu.append(item)
+		id+=1
 	return menu
 
 menu = read_from_db("JambaJuice")
-print(menu["Acai Primo Bowl"])
-=======
-    c.execute('SELECT * FROM ' + var)
-    #data = c.fetchall()
-    #print(data)
-    for row in c.fetchall():
-        print(row)
-
-
-read_from_db()
-#changes
->>>>>>> afe8f71bee686c14a3d80e3c916189a8fefe9fc4
+print(menu[10])
 c.close()
 conn.close()
 
