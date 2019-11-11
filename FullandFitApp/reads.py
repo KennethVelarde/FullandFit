1ae814f849997611aa1a2a3f34512bb3e07cfe5c
Nutella399 <nutella399@gmail.com>
2019-11-11 14:15:25 -0800

    read with a var

--- FullandFitApp/reads.py
+++ FullandFitApp/reads.py
@@ -1,10 +1,11 @@
 import sqlite3
 
 conn = sqlite3.connect('db.sqlite3')
+res = conn.execute("")
 c = conn.cursor()
 
-def read_from_db():
-    c.execute('SELECT * FROM JambaJuice')
+def read_from_db(var):
+    c.execute('SELECT * FROM ' + var)
     #data = c.fetchall()
     #print(data)
     for row in c.fetchall():
