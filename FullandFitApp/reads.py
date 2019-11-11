67dc9de5cac7b020928498788017b30028687b78
Nutella399 <nutella399@gmail.com>
2019-11-11 14:39:49 -0800

    read from db changes to a dictionary

--- FullandFitApp/reads.py
+++ FullandFitApp/reads.py
@@ -1,19 +1,28 @@
 import sqlite3
 
 conn = sqlite3.connect('db.sqlite3')
-res = conn.execute("")
 c = conn.cursor()
 
 def read_from_db(var):
-    c.execute('SELECT * FROM ' + var)
-    #data = c.fetchall()
-    #print(data)
-    for row in c.fetchall():
-        print(row)
+	menu = {}
+	c.execute('SELECT * FROM ' + var)
+	#data = c.fetchall()
+	#print(data)
+	for column in c.fetchall():
+		item = {}
+		item["name"] = column[0]
+		item["price"] = column[1]
+		item["calories"] = column[2]
+		item["carbs"] = column[3]
+		item["protein"] = column[4]
+		item["fat"] = column[5]
+		menu[column[0]] = item
+		#print(menu) 
+		#print(item)
+	return menu
 
-
-read_from_db()
-
+menu = read_from_db("JambaJuice")
+print(menu["Acai Primo Bowl"])
 c.close()
 conn.close()
 
