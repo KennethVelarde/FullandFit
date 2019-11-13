a945b51471c965d677d8a447d93c69047ab97b5a
Nutella399 <nutella399@gmail.com>
2019-11-12 18:46:28 -0800

    changed the reads functions to play better with the front end

--- FullandFitApp/reads.py
+++ FullandFitApp/reads.py
@@ -1,5 +1,49 @@
 import sqlite3
 
+def get_menu(var): 
+	string_var = str(var)
+	conn = sqlite3.connect('db.sqlite3')
+	c = conn.cursor()
+	c.execute('SELECT * FROM FullMenu WHERE id = ' + string_var) 
+	menu = []
+	for row in c.fetchall():
+		item = {}
+		item_id = 0
+		item["id"] = item_id
+		item["name"] = row[0]
+		item["price"] = row[2]
+		item["calories"] = row[3]
+		item["carbs"] = row[4]
+		item["protein"] = row[5]
+		item["fat"] = row[6]
+		item["egg"] = row[7]
+		item["milk"] = row[8]
+		item["peanut"] = row[9]
+		item["shellfish"] = row[10]
+		item["soy"] = row[11]
+		item["treenuts"] = row[12]
+		item["wheat"] = row[13]
+		menu.append(item)
+		item_id+=1
+	c.close()
+	conn.close()
+	return menu
+
+def get_restaurants(): 	
+	conn = sqlite3.connect('db.sqlite3')
+	c = conn.cursor()
+	restaurant = []
+	c.execute('SELECT * FROM restaurants')
+	for row in c.fetchall():
+		item = []
+		item.append(row[0]) 
+		item.append(row[1])
+		item.append(row[2])
+		restaurant.append(item)
+	c.close()
+	conn.close()
+	return restaurant
+
 def read_from_db(var):
 	conn = sqlite3.connect('db.sqlite3')
 	c = conn.cursor()
